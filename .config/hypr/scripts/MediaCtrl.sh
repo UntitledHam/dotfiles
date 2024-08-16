1624392123c3b9698b812c47850b7ff9cf78f043
untitledham <abemanfra@gmail.com>
2024-08-15 20:28:30 -0400

    added tofi

--- .config/hypr/scripts/MediaCtrl.sh
+++ .config/hypr/scripts/MediaCtrl.sh
@@ -5,25 +5,21 @@ music_icon="$HOME/.config/swaync/icons/music.png"
 # Play the next track
 play_next() {
     playerctl next
-    show_music_notification
 }
 
 # Play the previous track
 play_previous() {
     playerctl previous
-    show_music_notification
 }
 
 # Toggle play/pause
 toggle_play_pause() {
     playerctl play-pause
-    show_music_notification
 }
 
 # Stop playback
 stop_playback() {
     playerctl stop
-    notify-send -e -u low -i "$music_icon" "Playback Stopped"
 }
 
 # Display notification with song information
